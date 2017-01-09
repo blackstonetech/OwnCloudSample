508735615daa5a793fe7aefdf4fd8016e570145f
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2017-01-09 14:53:38 -0500

    updated sample app to owncloudsample

---
+++ lab4/app/config/app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ockyy6vl7wx0',
+  'passwordsalt' => 'eSpdeUurtKjMa29/CPQcxXYpqrsT+1',
+  'secret' => '5W+b5BKrudzRWnY+dUgUNMIPuGq0va7ZKwvy0V2kkogZ6wCu',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.3.149:8080',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://192.168.3.149:8080',
+  'dbtype' => 'mysql',
+  'version' => '9.1.3.1',
+  'dbname' => 'owncloud',
+  'dbhost' => 'mysql',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'rmb0s8dbXytzUaBJBzpOZirgmdby2W',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
---
+++ lab4/app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc96asawnx58',
+  'passwordsalt' => 'cXbb6U63q5PqJdYUlSNZl1cfSH/DTl',
+  'secret' => '7aKIjPzJwG7KdbklCHNLo7eqDspvXQ+G2zyBakj8TMYN3Isl',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost:8080',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://192.168.3.149:8080',
+  'dbtype' => 'mysql',
+  'version' => '9.1.3.1',
+  'dbname' => 'owncloud',
+  'dbhost' => 'mysql',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => '6MzC7oAbDlpEh68NDyfvUQWn6kKPW3',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
